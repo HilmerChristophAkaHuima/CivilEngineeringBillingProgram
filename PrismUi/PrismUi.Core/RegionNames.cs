5c8e4e3c1ab074d3d8d401b8177dbe55afe4ba24
Christoph Hilmer <hilmerchristoph@googlemail.com>
2020-09-27 03:35:45 +0200

    Add project files.

---
+++ PrismUi/PrismUi.Core/RegionNames.cs
@@ -0,0 +1,7 @@
+﻿namespace PrismUi.Core
+{
+    public static class RegionNames
+    {
+        public const string ContentRegion = "ContentRegion";
+    }
+}
---
+++ PrismUi/PrismUi/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Ioc;
+using PrismUi.Views;
+using System.Windows;
+using Prism.Modularity;
+using PrismUi.Modules.ModuleName;
+using PrismUi.Services.Interfaces;
+using PrismUi.Services;
+
+namespace PrismUi
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IMessageService, MessageService>();
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<ModuleNameModule>();
+        }
+    }
+}
